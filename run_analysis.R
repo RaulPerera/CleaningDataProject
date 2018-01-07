60a5f6d7c3c57baf82ba56a755b02fdfa0ec3ae5
Raul Perera <pererar@umich.edu>
2018-01-07 11:11:05 -0500

    Fixed error with binding columns prior to merging

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,11 @@
+##Helpful placeholder depending on what your current working directory is
+#getwd()
+#setwd("Dataset/UCI HAR Dataset")
+
+
 ###Dataset Source, unzipped outside of R
 #URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 #download.file(URL,"Dataset.zip")
-
-##Helpful placeholder depending on what your current working directory is
-#setwd("Dataset/UCI HAR Dataset")
 
 
 ## Create temporary datasets to store variable names and identify columns to keep
@@ -17,63 +19,71 @@ keepcolumns <- grepl("mean|std", lowercasenames)
 
 ##Import activitiy labels for future use
 activitylabels <- read.table("activity_labels.txt")
-names(activitylabels) <- c("activitytype","activity")
+names(activitylabels) <- c("activityID","activity")
 
-#Change Working directory to clean Train dataset
+#Change Working directory to clean train dataset
 setwd("train")
-## CLEAN TRAIN DATASET
-## 1. Read the Train dataset
+## CLEAN train DATASET
+## 1. Read the train dataset
 train <- read.table("X_train.txt")
 ## 2. Add column labels in order to appropriately label the raw dataset 
 ##    with appropriate variable names
 names(train) <- lowercasenames
 ## 3. Keep only columns containing mean or standard deviation of the measurement
 trainsubset <-train[,keepcolumns] 
-## 4.1 Read in activity labels and name the columns to enable merge
-temp_train_activity_labels<- read.table("Y_train.txt")
-names(temp_train_activity_labels) <- "activitytype"
-##4.2 Add descriptive activity labels to initial table
-full_train_labels <- merge(
-  temp_train_activity_labels, activitylabels, by.x="activitytype", by.y="activitytype"
-            )
+## 4 Read in activity labels and name the columns to enable merge
+train_activity_labels<- read.table("Y_train.txt")
+names(train_activity_labels) <- "activityID"
 ## 5. Read subject table into temp table and label it
 train_subjects <- read.table("subject_train.txt")
 names(train_subjects) <- "Subject"
 ## 6. Combine activity lables, subject labels, with measurement data
-full_train <- cbind(train_subjects,full_train_labels[,2],trainsubset)
-names(full_train)[2] <- "activitytype"
+combined_train <- cbind(train_subjects,train_activity_labels,trainsubset)
+##7 Add descriptive activity labels to initial table and convert into table data.frame
+combined_train_labels <- tbl_df(merge(
+  combined_train, activitylabels, by.x="activityID", by.y="activityID"
+))
+##8 Drop Activitiy ID column and move activity column to the beginning
+full_train <- combined_train_labels %>%
+  select(-activityID) %>%
+  select(activity, everything())
 
 ## Repeat train data set steps on test data set
 setwd("..")
+#Change Working directory to clean test dataset
 setwd("test")
-#1
+## CLEAN test DATASET
+## 1. Read the test dataset
 test <- read.table("X_test.txt")
-#2
+## 2. Add column labels in order to appropriately label the raw dataset 
+##    with appropriate variable names
 names(test) <- lowercasenames
-#3
+## 3. Keep only columns containing mean or standard deviation of the measurement
 testsubset <-test[,keepcolumns] 
-#4
-temp_test_activity_labels<- read.table("Y_test.txt")
-names(temp_test_activity_labels) <- "activitytype"
-full_test_labels <- merge(
-  temp_test_activity_labels, activitylabels, by.x="activitytype", by.y="activitytype"
-)
-#5
+## 4 Read in activity labels and name the columns to enable merge
+test_activity_labels<- read.table("Y_test.txt")
+names(test_activity_labels) <- "activityID"
+## 5. Read subject table into temp table and label it
 test_subjects <- read.table("subject_test.txt")
 names(test_subjects) <- "Subject"
-#6
-full_test <- cbind(test_subjects,full_test_labels[,2],testsubset)
-names(full_test)[2] <- "activitytype"
+## 6. Combine activity lables, subject labels, with measurement data
+combined_test <- cbind(test_subjects,test_activity_labels,testsubset)
+##7 Add descriptive activity labels to initial table and convert into table data.frame
+combined_test_labels <- tbl_df(merge(
+  combined_test, activitylabels, by.x="activityID", by.y="activityID"
+))
+##8 Drop Activitiy ID column and move activity column to the beginning
+full_test <- combined_test_labels %>%
+  select(-activityID) %>%
+  select(activity, everything())
 
 ## Combine test and train data set into one data.frame
 rundata <- rbind(full_train,full_test)
 
-##Convert rundata into data.table
-rundata_tbl <- tbl_df(rundata)
 
 ##Create 2nd tidy data set with the mean of means for each subject and activity
-rundata_mean <- rundata_tbl %>%
-  group_by(activitytype,Subject) %>%
+rundata_mean <- rundata %>%
+  group_by(activity,Subject) %>%
   summarise_all(.funs = c(mean="mean"))
 
 setwd("..")
